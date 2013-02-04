57454a0ae0cd96db6f38fd32ecb1dc3433de4400
Jason LaCarrubba <jayubba@gmail.com>
2013-02-04 15:46:49 -0500

    added mixed-case email test; handle before_save a bit differenly

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  before_save { |user| user.email = email.downcase }
+  before_save { self.email.downcase! }
 
   validates :name,  presence: true, length: { maximum: 50 }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
